2d10f2b3dac4aa1aeb729786935715766cec65cd
Bezael Pérez <bezael_e@hotmail.com>
2017-12-07 21:02:51 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }   from './app.component';
+import { UserComponent } from './component/user.component.ts';
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, UserComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'user',
   template: `
   		<h1>Hello {{name}}</h1>
 		<p>{{email}}</p>
